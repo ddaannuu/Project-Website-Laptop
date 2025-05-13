1187e45dc752c9dd8552af7c83652b2a0a29edaa
AryaBayu_23.11.5477 <aryabayu02122@gmail.com>
2025-05-13 21:26:25 +0700

    Penambahan Js Navbar Product

--- deskripsi.js
+++ deskripsi.js
@@ -14,3 +14,20 @@
     });
   });
 
+  //nav product  
+  const tabklik = document.querySelectorAll(".nav-link[data-tab]");
+  const KlikContents = document.querySelectorAll(".tab-content");
+
+  tabLinks.forEach(link => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      tabLinks.forEach(link => link.classList.remove("active"));
+      tabContents.forEach(content => content.classList.remove("active"));
+
+      this.classList.add("active");
+      const tabId = this.getAttribute("data-tab");
+      document.getElementById(tabId).classList.add("active");
+    });
+  });
+
