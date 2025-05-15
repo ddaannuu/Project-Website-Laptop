cfcff5c2de3875adf2288dc67f1e314632b8074b
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-05-15 19:12:50 +0700

    Perbaikan layout dengan collase dan header transparan ketika discroll

--- deskripsi.js
+++ deskripsi.js
@@ -31,3 +31,12 @@
     });
   });
 
+  window.addEventListener('scroll', function () {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {  // jika scroll lebih dari 50px
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  });
+  
