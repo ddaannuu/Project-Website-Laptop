e55df52f7e75e19d70b0d5353b75101ec0b8848c
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-07-08 16:22:56 +0700

    Penambahan file profile/danu

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -8,7 +8,7 @@ import Workstastion from '../views/Workstastion.vue'
 // import ProductDetail from '@/views/ProductDetail.vue'
 import Students from '@/views/Students.vue'
 import VideoEditing from '@/views/VideoEditing.vue'
-// import Profil_danu from '../views/Profil_danu.vue'
+import Profil_danu from '../views/Profil_danu.vue'
 // import Profil_baskara from '../views/Profil_baskara.vue'
 // import Profil_vianda from '../views/Profil_vianda.vue'
 // import Profil_farhan from '../views/Profil_farhan.vue'
@@ -36,8 +36,7 @@ const routes = [
 //{ path: '/product/:id', name: 'ProductDetail', component: ProductDetail },
   { path: '/students', name: 'Students', component: Students },
   { path: '/videoediting', name: 'VideoEditing', component: VideoEditing },
-
-//  { path: '/profil_danu', name: 'Profil_danu', component: Profil_danu },
+  { path: '/profil_danu', name: 'Profil_danu', component: Profil_danu },
 //   { path: '/profil_baskara', name: 'Profil_baskara', component: Profil_baskara },
 //   { path: '/profil_vianda', name: 'Profil_vianda', component: Profil_vianda },
 //   { path: '/profil_farhan', name: 'Profil_farhan', component: Profil_farhan },
@@ -45,17 +44,12 @@ const routes = [
 //   { path: '/profil_arya', name: 'Profil_arya', component: Profil_arya },
 //   { path: '/login', name: 'Login', component: Login, meta: { layout: 'auth' } },
 //   {  path: '/dashboard', name: 'DashboardAdmin', component: DashboardAdmin,     meta: { layout: 'auth', requiresAuth: true } },
-
 // { path: '/users/create', name: 'Create', component: CreateUser, meta: { layout:'auth', requiresAuth: true } },
 // { path: '/users/edit/:id', name: 'Edit', component: EditUser, meta: { layout:'auth', requiresAuth: true } },
 // { path: '/products/manage',name: 'ManageProduct',component: ManageProduct,meta: { layout: 'auth', requiresAuth: true } },
-
 // {path: '/products/create', name: 'CreateForm',compnent: CreateForm,meta: { layout: 'auth', requiresAuth: true }},
-
 // { path: '/products/edit/:id', name: 'EditNewArrival',component: EditNewArrival,meta: { layout: 'auth', requiresAuth: true }},
-
 // { path: '/products/edit_on_sale/:id',name: 'EditOnSale',component: EditOnSale,meta: { layout: 'auth', requiresAuth: true }},
-
 // {path: '/products/edit_best_seller/:id',name: 'EditBestSeller',component: EditBestSeller,meta: { layout: 'auth', requiresAuth: true }}
 
 
