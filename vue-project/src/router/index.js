084005d276950a7d5c45b93015b442eea2fec278
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-07-08 16:02:11 +0700

    Penambahan file categories/Bussines

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../views/HomePage.vue'
 import Categories from '../views/Categories.vue'
 // import Product from '../views/Product.vue'
-// import Business from '../views/Business.vue'
+import Business from '../views/Business.vue'
 import Workstastion from '../views/Workstastion.vue'
 // import ProductDetail from '@/views/ProductDetail.vue'
 // import Students from '@/views/Students.vue'
@@ -37,7 +37,7 @@ const routes = [
   { path: '/homepage', name: 'HomePage', component: HomePage },
   { path: '/categories', name: 'Categories', component: Categories },
 //   { path: '/products', name: 'Product', component: Product },
-//   { path: '/business', name: 'Business', component: Business },
+  { path: '/business', name: 'Business', component: Business },
   { path: '/workstastion', name: 'Workstastion', component: Workstastion },
 //   { path: '/product/:id', name: 'ProductDetail', component: ProductDetail },
 //   { path: '/students', name: 'Students', component: Students },
