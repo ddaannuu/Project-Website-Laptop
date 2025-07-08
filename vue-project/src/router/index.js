46ff8762722aa80b871761bb3b4f9fd5228e45da
I Made Baskara Saccid Ananda <imadebaskarasaccidananda@gmail.com>
2025-07-08 15:55:23 +0700

    Pemindahann dan penambahan file categories/workstastion

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -2,10 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 
 import HomePage from '../views/HomePage.vue'
-// import Categories from '../views/Categories.vue'
+import Categories from '../views/Categories.vue'
 // import Product from '../views/Product.vue'
 // import Business from '../views/Business.vue'
-// import Workstastion from '../views/Workstastion.vue'
+import Workstastion from '../views/Workstastion.vue'
 // import ProductDetail from '@/views/ProductDetail.vue'
 // import Students from '@/views/Students.vue'
 // import VideoEditing from '@/views/VideoEditing.vue'
@@ -35,10 +35,10 @@ const routes = [
 
   
   { path: '/homepage', name: 'HomePage', component: HomePage },
-//   { path: '/categories', name: 'Categories', component: Categories },
+  { path: '/categories', name: 'Categories', component: Categories },
 //   { path: '/products', name: 'Product', component: Product },
 //   { path: '/business', name: 'Business', component: Business },
-//   { path: '/workstastion', name: 'Workstastion', component: Workstastion },
+  { path: '/workstastion', name: 'Workstastion', component: Workstastion },
 //   { path: '/product/:id', name: 'ProductDetail', component: ProductDetail },
 //   { path: '/students', name: 'Students', component: Students },
 //   { path: '/videoediting', name: 'VideoEditing', component: VideoEditing },
